aaa1be081c1cc8e50a78201e3a5c014793405b93
dikaizm <dikamaah@gmail.com>
2026-03-12 10:57:05 +0700

    Add gradient clipping to prevent exploding gradients during training

--- geoai/train.py
+++ geoai/train.py
@@ -3380,6 +3380,7 @@ def train_semantic_one_epoch(
         # Backward pass
         optimizer.zero_grad()
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
 
         # Track loss
